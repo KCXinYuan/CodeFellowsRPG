7c0442c7d9154460f4adab1af7a9b9c6c0302fb3
Kevin Chuang <kcxinkun@gmail.com>
2016-02-24 19:52:34 -0800

    name change based on type, buff charlie, monster stats scaled

--- client/scripts/monsFact.js
+++ client/scripts/monsFact.js
@@ -1,7 +1,7 @@
 function Enemy (hp,attack,type,sprite) {
-  this.currentHP = hp;
-  this.maxHP = hp;
-  this.attack = attack;
+  this.currentHP = hp*50;
+  this.maxHP = hp*50;
+  this.attack = attack*5;
   this.type = type;
   this.sprite = sprite;
 }
